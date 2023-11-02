e8811f4c50c95a73e8335d026cd9d507cedbddbb
mickalap <mickalapus@gmail.com>
2023-11-02 23:13:28 +0100

    data agumentation v1

--- data_augmentation/data_augmentation_tool.py
+++ data_augmentation/data_augmentation_tool.py
@@ -6,14 +6,6 @@ Created on Tue Oct 31 20:44:46 2023
 @author: michal
 """
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 31 19:43:55 2023
-
-@author: michal
-"""
-
 import numpy as np
 from queue import Queue
 from scipy.spatial import ConvexHull, Delaunay
@@ -42,35 +34,58 @@ import numba
 from pycaret.regression import *
 import lightgbm as lgb
 from scipy import *
-#%%
-df = pd.read_excel("/Users/michal/OneDrive - University of Gdansk/OneDrive - University of Gdansk (for Students)/irini/data_with_descriptors.xlsx")
-df.head()
+from sklearn import datasets
 
-#%%
-df["cell viability _(%)"] = df["cell viability _(%)"].replace({"?": np.nan}).astype(float)
-df.loc[df["cell viability _(%)"] > 130, "cell viability _(%)"] = 130  # limit values to max 130
-df.dropna(subset=["cell viability _(%)"], inplace=True)
-df_numeric = df.select_dtypes(exclude=[object])
-df_numeric.drop(columns=["Exposure dose_PPM", "cell viability _(%)"], inplace=True)
 
-decomposer = TruncatedSVD(n_components=5)
-transformed_data = decomposer.fit_transform(df_numeric)
-explained_variances = decomposer.explained_variance_ratio_
-plt.plot(explained_variances)
+iris = datasets.load_iris()
+data = iris.data, iris.feature_names
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
 
-df.select_dtypes(exclude=[object]).shape
-df_numeric.describe()
-df_numeric.describe().loc["std", :]
-df.select_dtypes(exclude=[object]).head()
 
-X = df.select_dtypes(exclude=[object])
-y = X["cell viability _(%)"].values
-X = X.drop(columns=["Exposure dose_PPM", "cell viability _(%)"])
 
-selected_features = ["HOMO energy [eV]", "LUMO energy [eV]", "Band gap [eV]", "HOMO boundries [eV]", "Dipole moment", "Sp"] + ["Exposure dose_PPM"]
+mapper = umap.UMAP(n_components=3, metric="minkowski", n_neighbors=20, min_dist=1.0)
+mapping = mapper.fit_transform(df)
 
-py_reg.setup(df[selected_features + ["cell viability _(%)"]],
-             target="cell viability _(%)",
+
+
+hull = ConvexHull(mapping)
+
+for simplex in hull.simplices:
+    plt.plot(mapping[simplex, 0], mapping[simplex, 1], 'k-')
+
+def poisson_disc_sampling(points, radius=0.1):
+    # Generate the Poisson disk samples within the convex hull
+    tri = Delaunay(points)
+    lb, ub = tri.min_bound, tri.max_bound
+    size = np.abs(ub - lb)
+    poisson = PoissonDisk(d=3, radius=radius)
+    unit_samples = poisson.fill_space()
+    samples = lb + size * unit_samples
+
+    # Select the points that are within the convex hull
+    simplex = tri.find_simplex(samples)
+    samples = samples[simplex >= 0]
+    return samples
+
+
+# Generate random points in 3D space
+points = mapping
+
+# Compute the convex hull
+hull = ConvexHull(points)
+
+# Compute the Delaunay triangulation of the hull
+tri = Delaunay(points)
+
+# generate poisson disk samples within the convex hull
+samples = poisson_disc_sampling(points, radius=0.1)
+
+inverse_samples = mapper.inverse_transform(samples)
+data_new = pd.concat([df, pd.DataFrame(inverse_samples, columns=df.columns)]).reset_index(drop=True)
+
+
+py_reg.setup(df,
+             target="petal width (cm)",
              preprocess=True,
              feature_selection=False,
              feature_selection_method="sequential",
@@ -83,35 +98,25 @@ py_reg.setup(df[selected_features + ["cell viability _(%)"]],
              pca=False,
              pca_method="linear",
              pca_components=6,
-             profile=False,
-             use_gpu=True)
+             profile=False)
 
-X = py_reg.get_config("X")
-y = py_reg.get_config("y")
+best = py_reg.compare_models()
 
-X_train = py_reg.get_config("X_train")
-y_train = py_reg.get_config("y_train")
+py_reg.setup(data_new,
+             target="petal width (cm)",
+             preprocess=True,
+             feature_selection=False,
+             feature_selection_method="sequential",
+             n_features_to_select=20,
+             normalize=False,
+             normalize_method="robust",
+             remove_multicollinearity=True,
+             multicollinearity_threshold=0.95,
+             low_variance_threshold=0.1,
+             pca=False,
+             pca_method="linear",
+             pca_components=6,
+             profile=False)
 
-X_val = py_reg.get_config("X_test")
-y_val= py_reg.get_config("y_test")
-
-print(f"{X_train.shape[1]} features after processing")
-
-X_train.head()
-
-#%%
-
-mapper = umap.UMAP(n_components=3, metric="minkowski", n_neighbors=100, min_dist=1.0)
-data = X.join(y)
-mapping = mapper.fit_transform(data)
-
-
-
-
-
-
-
-
-
-
+best = py_reg.compare_models()
 
